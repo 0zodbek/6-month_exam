c505cbd925407b8a67f5e93d6dc5494d6add45a6
0zodbek <ozpdbekov@gmail.com>
2024-08-09 09:34:54 +0500

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,37 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Error from './pages/ErrorPage/Error.jsx'
 import Login from './pages/LoginPage/Login.jsx'
-import Register from './pages/RegisterPage/Regiser.jsx'
+import Register from "./pages/RegisterPage/Regiser.jsx"
+
 function App() {
-  const [count, setCount] = useState(0)
   const [isAuth, setIsAuth] = useState(false);
-  const [token, setToken] = useState(localStorage.getItem('token'));
-  useEffect(
-    function(){
-    if(token){
+  const navigate = useNavigate();
+  let token = JSON.parse(localStorage.getItem("token"));
+  useEffect(() => {
+    if (token) {
       setIsAuth(true);
-    }else{
-      setIsAuth(false)
-      if( !location.pathname.includes('register')){
-      navigate("/login")
+    } else {
+      setIsAuth(false);
+      if (!window.location.pathname.includes('register')) {
+        navigate("/login");
       }
     }
-    },
-    [token, location.pathname]
-    );
+  }, [token, navigate]);
+
   return (
     <Routes>
-      <Route path='/' element={<Login></Login>}></Route>
-      <Route path='/register' element={<Register></Register>}></Route>
-      isAuth && {" "}
-        
-          <>
-          {console.log("salom1")}
-            <Route path='/home' element={<Home></Home>}></Route>
-            {console.log("salom2")}
-          </>
-        
-
-      <Route path='*' element={<Error></Error>}></Route>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      {isAuth && (
+        <>
+          <Route path='/' element={<Home />} />
+        </>
+      )}
+      <Route path='*' element={<Error />} />
     </Routes>
   )
 }
